7c7076cb3b73c3ac4b1a21d3df505993205f80c6
DinhTheVu <145786147+DinhTheVu@users.noreply.github.com>
2025-06-05 21:24:38 +0700

    Remove .nuxt folder from tracking

--- HopeBox.UI/.nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from 'E:/EXE/HopeBox/HopeBox.UI/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default defuFn(inlineConfig)
--- HopeBox.UI/.nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///E:/EXE/HopeBox/HopeBox.UI/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
