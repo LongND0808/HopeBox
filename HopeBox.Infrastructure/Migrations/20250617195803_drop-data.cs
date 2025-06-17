43344c480342ac888377a33c2b366f1faec7b6d8
LongND0808 <long2003.2014@gmail.com>
2025-06-18 02:59:13 +0700

    drop and re-seeding data

---
+++ HopeBox.Infrastructure/Migrations/20250617195803_drop-data.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HopeBox.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class dropdata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM DonationReliefPackages");
+            migrationBuilder.Sql("DELETE FROM ReliefPackageItems");
+            migrationBuilder.Sql("DELETE FROM Donations");
+            migrationBuilder.Sql("DELETE FROM ReliefItems");
+            migrationBuilder.Sql("DELETE FROM ReliefPackages");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
