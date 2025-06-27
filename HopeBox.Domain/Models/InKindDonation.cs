a3fd208e973af66bb00720ef9beb28b9d289208b
LongND0808 <long2003.2014@gmail.com>
2025-06-27 08:56:29 +0700

    update inkind

--- HopeBox.Domain/Models/InKindDonation.cs
+++ HopeBox.Domain/Models/InKindDonation.cs
@@ -1,28 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static HopeBox.Common.Enum.Enumerate;
 
 namespace HopeBox.Domain.Models
 {
-    public class InKindDonation : BaseModel
+    public class InkindDonation : BaseModel
     {
         public Guid UserId { get; set; }
 
+        public Guid? ApprovedBy { get; set; }
+
         public Guid EventId { get; set; }
 
-        public string Description { get; set; }
+        public string InKind { get; set; } = String.Empty;
 
-        public DateTime CreatedDate { get; set; }
+        public DateTime DonationDate { get; set; }
 
-        public string SenderName { get; set; }
+        public DateTime? ApprovedDate { get; set; }
 
-        public string From { get; set; }
+        public DateTime? DeliveredDate { get; set; }
 
-        public string To { get; set; }
+        public DateTime? EstimatedDeliveryDate { get; set; }
 
+        public InkindDonationStatus Status { get; set; }
+
+        public bool IsAnonymous { get; set; }
+
+        [MaxLength(500)]
+        public string? Message { get; set; }
+
+        [Required, MaxLength(200)]
+        public required string SenderAddress { get; set; }
+
+        [Required, MaxLength(200)]
+        public required string ReceiverAddress { get; set; }
+
+        [ForeignKey(nameof(UserId))]
         public virtual User? User { get; set; }
+
+        [ForeignKey(nameof(ApprovedBy))]
+        public virtual User? Approver { get; set; }
+
         public virtual Event? Event { get; set; }
     }
 }
